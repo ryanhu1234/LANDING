aa9378fbc69216e84dad1387cfd74b6ecd17972a
ryanhu1234 <46936593+ryanhu1234@users.noreply.github.com>
2019-03-19 11:38:03 -0400

    Update calc.js

--- calc.js
+++ calc.js
@@ -15,7 +15,11 @@ function getNewLine() {
 
 }
 
-pagecode = 'print ("This is a basic four function calculator")
+pagecode = 'import math
+while 1 == 1:
+  cho1 = input("normal or trig ")
+  if cho1 == \'normal\':
+    print ("This is a basic four function calculator")
 
     #define the operations
     def add (x, y):return x + y
@@ -46,7 +50,44 @@ pagecode = 'print ("This is a basic four function calculator")
     elif typ == \'/\':
         print (divide (in2, in3))
     elif typ != \'+\' or \'-\' or \'*\' or \'/\':
-        print("Nonexistant operation!")'
+        print("Nonexistant operation!")
+  elif cho1 == \'trig\':
+    cho2 = input(\'convert or functions \')
+    if cho2 == \'convert\':
+      #asks what calc they want
+      print ("What would you like to convert")
+
+      #takes input and populates variable conchoice and numconvert
+      conchoice = str(input(\'r to d or d to r \'))
+      numconvert = float(input("what value would you like to convert "))
+
+    #uses variable q and e to choose between math.degrees and math.radians
+      if conchoice == \'r to d\':
+          print (math.degrees(numconvert))
+      elif conchoice == \'d to r\':
+         print (math.radians(numconvert))
+      elif conchoice != \'d to r\' or \'r to d\':
+          print ("You typed an incorrect choice!")
+    if cho2 == \'functions\':
+      #outputs options
+      print ("This is a Cosine, Sine, and Tangent calculator")
+      print(\'cos\')
+      print(\'sin\')
+      print(\'tan\')
+
+       #inputs
+      func = str(input(\'What trig function would you like \'))
+      inval = float(input("Input value "))
+
+      #does the selected option and uses variable var 
+      if func == \'cos\' :
+          print (math.cos(inval))
+      elif func == \'sin\' :
+          print (math.sin(inval))
+      elif func == \'tan\' :
+          print (math.tan(inval))
+      elif func != \'cos\' or \'sin\' or \'tan\':
+          print ("You typed an incorect value or function type!")'
 
 document.write(pagecode);
 
